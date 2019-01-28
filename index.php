d57cdf7c073cca058768e95467362a0c23e2b23b
ehfitrias <43806677+ehfitrias@users.noreply.github.com>
2019-01-28 14:58:54 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>My first PHP page</h1>
+
+<?php
+$ok1 = "Superman";
+$ok2 = "Batman";
+
+echo "Hello World! <br>";
+echo "How are you today? <br>";
+echo $ok1;
+echo " = i am good <br>";
+echo $ok2;
+echo " = i am also good <br>";
+?> 
+
+</body>
+</html>
